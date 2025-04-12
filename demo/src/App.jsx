4bfb8d1f70568d79c9047a0465b56770876e2268
Harsh Haria <harshharia1234@gmail.com>
2025-04-13 04:43:10 +0530

    demo project basic update

---
+++ demo/src/App.jsx
@@ -0,0 +1,21 @@
+import raectImg from "./assets/react-core-concepts.png";
+
+function App() {
+	return (
+		<div>
+			<header>
+				<img src={raectImg} alt="Stylized atom" />
+				<h1>React Essentials</h1>
+				<p>
+					{1 + 1} React concepts you will need for almost any app you are going
+					to build!
+				</p>
+			</header>
+			<main>
+				<h2>Time to get started!</h2>
+			</main>
+		</div>
+	);
+}
+
+export default App;
