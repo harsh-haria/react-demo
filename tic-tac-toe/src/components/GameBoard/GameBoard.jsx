6087018fa4542c90a27c7016536926d7ec855b22
Harsh Haria <harshharia1234@gmail.com>
2025-05-15 23:48:47 +0530

    updated tic-tac-toe: Game now functional. Yet to have logic for win

--- tic-tac-toe/src/components/GameBoard/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard/GameBoard.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
+
 let initialGameBoard = [
-    ['X', null, null],
-    [null, null, 'O'],
-    [null, 'X', null]
-]
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+];
+
+let initalPlayer = true;
 
 export default function GameBoard() {
-    return (
-			<ol id="game-board">
-				{initialGameBoard.map((row, _rowIndex) => (
-					<li key={_rowIndex}>
-						<ol>
-							{row.map((col, _colIndex) => (
-								<li>
-									<button key={_colIndex}>{col}</button>
-								</li>
-							))}
-						</ol>
-					</li>
-				))}
-			</ol>
-		);
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+	const [playerTurn, setPlayerTurn] = useState(initalPlayer);
+
+	function UpdateBoard(row, col) {
+		if (gameBoard[row][col]) {
+			return;
+		}
+		let character = playerTurn ? 'X' : 'O';
+		let newGameBoard = [...gameBoard.map(innerArray => [...innerArray])];
+		newGameBoard[row][col] = character;
+		setGameBoard(newGameBoard);
+		setPlayerTurn(playerTurn => !playerTurn);
+	}
+
+	return (
+		<ol id="game-board">
+			{gameBoard.map((row, _rowIndex) => (
+				<li key={_rowIndex}>
+					<ol>
+						{row.map((col, _colIndex) => (
+							<li key={_colIndex}>
+								<button onClick={() => UpdateBoard(_rowIndex, _colIndex)}>
+									{col}
+								</button>
+							</li>
+						))}
+					</ol>
+				</li>
+			))}
+		</ol>
+	);
 }
